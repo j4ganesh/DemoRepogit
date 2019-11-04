a655edc9f4cfac9ec9575d66a6e62ed6b945c133
j4ganesh <j4ganesh@gmail.com>
2019-11-04 10:06:26 +0000

    modi

---
+++ ShellConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShellConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Shell Petrol card bussines portal");
+            Console.ReadKey();
+        }
+    }
+}
